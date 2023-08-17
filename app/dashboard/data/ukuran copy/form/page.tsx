9f57e6811f89a8c444cf86180a075ce1989d2ec2
sarrahman-me <sarrahman.me@gmail.com>
2023-08-17 14:18:30 +0700

    :art: perbaikan berdasarkan sonarlint

---
+++ app/dashboard/data/ukuran copy/form/page.tsx
@@ -0,0 +1,21 @@
+import react from "react";
+import { HeaderAndBackIcon } from "@/src/components/molecules";
+import { FormData } from "@/src/template";
+
+export default function FormUkuran() {
+  const form = [
+    {
+      type: "text",
+      label: "Nama Ukuran",
+      name: "nama_ukuran",
+      autoFocus: true,
+    },
+  ];
+
+  return (
+    <div>
+      <HeaderAndBackIcon title="Tambah Ukuran" />
+      <FormData submitEndpoint={"/products/ukuran"} formInput={form} />
+    </div>
+  );
+}
