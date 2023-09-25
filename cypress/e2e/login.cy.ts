a9b42604fd9a3ed14ccfa3e1e525b3ceac128544
sarrahman-me <sarrahman.me@gmail.com>
2023-09-25 09:02:58 +0700

    :test_tube: menambahkan e2e testing dengang cypress

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,37 @@
+describe("template spec", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/login");
+  });
+
+  it("tampilan wajib dari halaman ini", () => {
+    cy.contains("Dunia Bangunan").should("exist");
+    cy.get('[for="username"]').should("exist");
+    cy.get('[for="password"]').should("exist");
+    cy.contains("Masuk").should("exist");
+    cy.contains("Belum punya akun").should("exist");
+  });
+
+  it("Login harus gagal", () => {
+    // Mengisi formulir login dengan informasi yang salah
+    cy.get('[for="username"]').type("invalid-username");
+    cy.get('[for="password"]').type("invalid-password");
+
+    // Menyimulasikan pengiriman formulir
+    cy.get("form").submit();
+
+    // Memeriksa pesan akun tidak ditemukan
+    cy.contains("Akun tidak ditemukan").should("exist");
+  });
+
+  it("login harus berhasil", () => {
+    // Mengisi formulir login
+    cy.get('[for="username"]').type("superadmin");
+    cy.get('[for="password"]').type("secret");
+
+    // Menyimulasikan pengiriman formulir
+    cy.get("form").submit();
+
+    // Memeriksa pesan login berhasil
+    cy.contains("Login Berhasil").should("exist");
+  });
+});
---
+++ cypress/e2e/register.cy.ts
@@ -0,0 +1,46 @@
+describe("template spec", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/register");
+  });
+
+  it("tampilan wajib dari halaman ini", () => {
+    cy.contains("Sarrahman Bangunan").should("exist");
+    cy.get('[for="nama"]').should("exist");
+    cy.get('[for="username"]').should("exist");
+    cy.get('[for="email"]').should("exist");
+    cy.get('[for="whatsapp"]').should("exist");
+    cy.get('[for="password"]').should("exist");
+    cy.contains("Daftar").should("exist");
+    cy.contains("Sudah punya akun").should("exist");
+  });
+
+//   it("login harus berhasil", () => {
+//     // Mengisi formulir login dengan informasi yang salah
+//     cy.get('[for="nama"]').type("superadmin");
+//     cy.get('[for="username"]').type("superadmin");
+//     cy.get('[for="email"]').type("superadmin@gmail.com");
+//     cy.get('[for="whatsapp"]').type("08123456789");
+//     cy.get('[for="password"]').type("secret");
+
+//     // Menyimulasikan pengiriman formulir
+//     cy.get("form").submit();
+
+//     // Memeriksa pesan akun tidak ditemukan
+//     cy.contains("Pendaftaran user berhasil").should("exist");
+//   });
+
+  it("Register harus gagal (username sudah digunakan)", () => {
+    // Mengisi formulir login dengan informasi yang salah
+    cy.get('[for="nama"]').type("superadmin");
+    cy.get('[for="username"]').type("superadmin");
+    cy.get('[for="email"]').type("superadmin@gmail.com");
+    cy.get('[for="whatsapp"]').type("08123456789");
+    cy.get('[for="password"]').type("secret");
+
+    // Menyimulasikan pengiriman formulir
+    cy.get("form").submit();
+
+    // Memeriksa pesan akun tidak ditemukan
+    cy.contains("Username sudah digunakan").should("exist");
+  });
+});
