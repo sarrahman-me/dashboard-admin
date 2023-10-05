1c4470238400c43f00a1a17c972ed041f8ccd44d
sarrahman-me <sarrahman.me@gmail.com>
2023-10-05 16:29:17 +0700

    :memo: memperbaiki alur deployment situs

--- layouts/template/timeline/index.tsx
+++ layouts/template/timeline/index.tsx
@@ -38,6 +38,7 @@ export default function Timeline(props: {
   return (
     <div className="bg-white dark:bg-slate-800 rounded-md borde p-2 my-2 w-full shadow m-1">
       <ol className="relative border-l border-gray-200 dark:border-gray-700">
+        {/* langkah 1 */}
         <li className="mb-10 ml-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
           <h3 className="text-base font-semibold text-gray-900 dark:text-white">
@@ -48,6 +49,8 @@ export default function Timeline(props: {
           </p>
           <Button href="https://app.netlify.com/start">Netlify</Button>
         </li>
+
+        {/* langkah 2 */}
         <li className="mb-10 ml-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
           <h3 className="text-base font-semibold text-gray-900 dark:text-white">
@@ -63,9 +66,19 @@ export default function Timeline(props: {
             <li>Publish directory: .next</li>
           </ul>
           <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-            Setelah selesai klik Deploy
+            Menambahkan environment variable pada situs webstore, tambahkan key
+            value sebagai berikut
           </p>
+          <ul className="my-2 bg-slate-100 dark:bg-slate-900 rounded-md">
+            <li>NEXT_PUBLIC_HOST=https://api.tokokeramik.com</li>
+            <li>
+              NEXT_PUBLIC_ID_WEBSTORE=
+              {props.id_webstore}
+            </li>
+          </ul>
         </li>
+
+        {/* langkah 3 */}
         <li className="mb-10 ml-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
           <h3 className="text-base font-semibold text-gray-900 dark:text-white">
@@ -83,36 +96,56 @@ export default function Timeline(props: {
             )}
           </div>
         </li>
+
+        {/* langkah 4 */}
         <li className="mb-10 ml-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
           <h3 className="text-base font-semibold text-gray-900 dark:text-white">
             Langkah 4
           </h3>
           <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-            Menambahkan environment variable pada situs webstore, tambahkan key
-            value sebagai berikut
+            Kunjungi file docker-compose.yml di github dan tambahkan nama domain
+            di bawah ini ke environment variable ALLOWED_DOMAIN
           </p>
-          <ul className="my-2 bg-slate-100 dark:bg-slate-900 rounded-md">
-            <li>NEXT_PUBLIC_HOST=https://api.tokokeramik.com</li>
-            {props.nama_webstore ? (
-              <li>
-                NEXT_PUBLIC_DOMAIN=
-                {`https://${stringToSlug(props.nama_webstore)}.netlify.app`}
-              </li>
-            ) : (
-              <div>
-                <li>NEXT_PUBLIC_DOMAIN=https://[nama_webstore].netlify.app</li>
-                <p className="text-xs">
-                  Ganti [nama_webstore] sesuai dengan nama webstore
-                </p>
-              </div>
-            )}
-          </ul>
+          {props.nama_webstore && (
+            <ul className="my-2 bg-slate-100 dark:bg-slate-900 rounded-md">
+              <li>{`https://${stringToSlug(
+                props.nama_webstore
+              )}.netlify.app`}</li>
+            </ul>
+          )}
+          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+            Kunjungi file Github dengan tombol di bawah
+          </p>
+          {props.nama_webstore && (
+            <Button
+              href={`https://github.com/sarrahman-bangunan/docker-compose`}
+            >
+              Github
+            </Button>
+          )}
         </li>
+
+        {/* langkah 5 */}
         <li className="mb-10 ml-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
           <h3 className="text-base font-semibold text-gray-900 dark:text-white">
             Langkah 5
+          </h3>
+          <p className="text-base font-normal text-gray-500 dark:text-gray-400">
+            Kunjungi Dasboard AWS untuk merestart server
+          </p>
+          <p className="text-xs text-red-500">
+            pastikan kamu melakukan ini di tengah malam agar tidak ada yang
+            terganggu jika server down
+          </p>
+        </li>
+
+        {/* langkah 6 */}
+        <li className="mb-10 ml-4">
+          <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+          <h3 className="text-base font-semibold text-gray-900 dark:text-white">
+            Langkah 6
           </h3>
           <p className="text-base font-normal text-gray-500 dark:text-gray-400">
             Kunjungi webstore dan lakukan pengujian dengan mengklik tombol
@@ -126,10 +159,12 @@ export default function Timeline(props: {
             </Button>
           )}
         </li>
+
+        {/* langkah 7*/}
         <li className="mb-10 ml-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
           <h3 className="text-base font-semibold text-gray-900 dark:text-white">
-            Langkah 6
+            Langkah 7
           </h3>
           <p className="text-base font-normal text-gray-500 dark:text-gray-400">
             Lakukan verifikasi untuk menyelesaikan proses Deployment
