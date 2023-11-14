b15f362d11333d48d1934ce80cdcf7c7c21ae1b4
sarrahman-me <sarrahman.me@gmail.com>
2023-11-14 09:04:09 +0800

    :hammer: memperbaiki halaman dashboard insight

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -38,57 +38,62 @@ export default function Dashboard() {
 
   return (
     <div>
-      <Typography variant="subtitle">Insight</Typography>
+      <Typography variant="subtitle">Insight (Kemarin)</Typography>
       <div className="flex items-center my-2 space-x-3">
-        <div className="p-2 rounded bg-white dark:bg-gray-800">
-          <Typography>{dailySearch.total_searches} pencarian</Typography>
+        <div className="flex flex-col p-2 rounded bg-white dark:bg-gray-800">
+          <Typography align="center">{dailySearch.total_searches}</Typography>
           <Typography variant="helper" color="secondary">
-            kemarin
+            pencarian
           </Typography>
         </div>
-        <div className="p-2 rounded bg-white dark:bg-gray-800">
-          <Typography>
-            {dailyProductView.total_product_views} klik produk
+        <div className="flex flex-col p-2 rounded bg-white dark:bg-gray-800">
+          <Typography align="center">
+            {dailyProductView.total_product_views}
           </Typography>
           <Typography variant="helper" color="secondary">
-            kemarin
+            Produk Dilihat
           </Typography>
         </div>
       </div>
 
-      <div className="p-2 my-2 rounded bg-white dark:bg-gray-800">
-        <Typography variant="helper" color="secondary">
-          Keyword pencarian populer
-        </Typography>
-        <div className="my-1">
-          {dailySearchTrend.map((item: any, i: number) => (
-            <div key={i} className="flex items-center space-x-1">
-              <Typography>{item.query}</Typography>
-              <Typography>{item.total_search}</Typography>
-            </div>
-          ))}
+      <div className="flex items-center gap-2 flex-col md:flex-row">
+        <div className="p-2 rounded bg-white dark:bg-gray-800 md:w-2/3 w-full">
+          <Typography variant="helper" color="secondary">
+            Keyword pencarian populer
+          </Typography>
+          <div className="my-1 space-y-1">
+            {dailySearchTrend.map((item: any, i: number) => (
+              <div
+                key={i}
+                className="flex items-center justify-between border-b last:border-none"
+              >
+                <Typography>{item.query}</Typography>
+                <Typography>{item.total_search} x</Typography>
+              </div>
+            ))}
+          </div>
         </div>
-        <Typography variant="helper" color="secondary">
-          kemarin
-        </Typography>
-      </div>
 
-      <div className="p-2 rounded bg-white dark:bg-gray-800">
-        <Typography variant="helper" color="secondary">
-          Produk dilihat terbanyak
-        </Typography>
-        <div className="my-1">
-          {dailyPopulerProducts.map((item: any, i: number) => (
-            <div key={i} className="flex items-center space-x-1">
-              <Typography>{item.nama_barang}</Typography>
-              <Typography>{item.brand}</Typography>
-              <Typography>{item.viewed} x</Typography>
-            </div>
-          ))}
+        <div className="p-2 rounded bg-white dark:bg-gray-800 md:w-1/3 w-full">
+          <Typography variant="helper" color="secondary">
+            Produk dilihat terbanyak
+          </Typography>
+          <div className="my-1 space-y-1">
+            {dailyPopulerProducts.map((item: any, i: number) => (
+              <div
+                key={i}
+                className="flex items-center justify-between border-b last:border-none"
+              >
+                <span className="flex items-center space-x-1">
+                  <Typography>{item.nama_barang}</Typography>
+                  <Typography>{item.brand}</Typography>
+                  <Typography>{item?.warna}</Typography>
+                </span>
+                <Typography>{item.viewed} x</Typography>
+              </div>
+            ))}
+          </div>
         </div>
-        <Typography variant="helper" color="secondary">
-          kemarin
-        </Typography>
       </div>
     </div>
   );
