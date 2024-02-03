1bcd432e3b8e7561fa8584ec4fbb0c829758c2b9
sarrahman-me <sarrahman.me@gmail.com>
2024-02-03 19:39:53 +0700

    :hammer: menambahkan detail uptime untuk informasi server

--- app/dashboard/system/monitoring/page.tsx
+++ app/dashboard/system/monitoring/page.tsx
@@ -2,11 +2,13 @@
 import React, { useEffect, useState } from "react";
 import { GaugeChartComp } from "@/src/components";
 import { GetDataApi } from "@/src/utils";
+import moment from "moment";
 import CPUCalculator from "@/src/utils/cpuCalculator";
 
 export default function Monitoring() {
   const [cpu, setCpu] = useState<any>(0);
   const [memoryUsage, setMemoryUsage] = useState<any>(0);
+  const [uptime, setUptime] = useState<any>(0);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,6 +20,12 @@ export default function Monitoring() {
         const responseMemory = await GetDataApi(
           `${process.env.NEXT_PUBLIC_HOST}/monitoring/memory`
         );
+
+        const { uptime } = await GetDataApi(
+          `${process.env.NEXT_PUBLIC_HOST}/monitoring/uptime`
+        );
+
+        setUptime(uptime);
 
         const { totalMemory, freeMemory } = responseMemory;
 
@@ -33,14 +41,22 @@ export default function Monitoring() {
 
     fetchData();
 
-    const intervalId = setInterval(fetchData, 60000);
+    const intervalId = setInterval(fetchData, 5000);
 
     return () => clearInterval(intervalId);
   }, []);
 
+  // Format uptime using Moment.js
+  const duration = moment.duration(uptime, "seconds");
+  const formattedUptime = `${duration.hours()} jam ${duration.minutes()} menit ${duration.seconds()} detik`;
+
   return (
     <div>
-      <p className="font-bold text-lg">Monitoring</p>
+      <p className="font-bold text-center text-lg">Informasi Server</p>
+      <p className="font-semibold text-lg text-center my-3 text-lime-500">
+        {formattedUptime}
+      </p>
+
       <div className="justify-center space-x-3 flex">
         <div>
           <GaugeChartComp data={cpu} />
